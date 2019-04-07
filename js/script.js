a9846edb8782dbe859230769d5a09bd811e33489
Josh W <jtwong10@gmail.com>
2019-04-07 12:14:23 -0700

    Update

--- js/script.js
+++ js/script.js
@@ -1,26 +1,30 @@
+var db = firebase.firestore();
+
 function addData(){
     alert("Thank you for your comments!");
     var fname = document.getElementById("fname").value;
     var lname = document.getElementById("lname").value;
     var rating = document.getElementById("rating").value;
     var comments = document.getElementById("comments").value;
-    document.getElementById("commentbox").reset();
-    document.getElementById("submit").addEventListener("click", reloadPage());
+
+    console.log(rating);
+
+
+
     db.collection("comments").add({
         firstname: fname,
         lastname: lname,
         rating: rating,
         comments: comments
-    }
+    })
     .then(function(docRef) {
         console.log("Document written with ID: ", docRef.id);
-        getProducts();
         })
-        .catch(function(error) {
-            console.error("Error adding comment: ", error);
-        })
-    );
+    .catch(function(error) {
+        console.error("Error adding comment: ", error);
+    });
 }
+
 
 
 function reloadPage()
@@ -28,3 +32,6 @@ function reloadPage()
     window.location.reload()
 }
 
+
+// document.getElementById("commentbox").reset();
+// document.getElementById("submit").addEventListener("click", reloadPage());
